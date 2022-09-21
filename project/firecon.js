10523d31d4445be7618e133bb20c59c30a52548f
Santhosh22082 <114060627+Santhosh22082@users.noreply.github.com>
2022-09-21 03:02:49 -0600

    Add files via upload

---
+++ project/firecon.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC3yWZiYT2WXCUCOpX4Ye7sd763zynC18Y",
+    authDomain: "newone-c72d6.firebaseapp.com",
+    databaseURL: "https://newone-c72d6-default-rtdb.firebaseio.com",
+    projectId: "newone-c72d6",
+    storageBucket: "newone-c72d6.appspot.com",
+    messagingSenderId: "479209842825",
+    appId: "1:479209842825:web:dd2fc69a13d1d027243e58",
+    measurementId: "G-F66JBBGW24"
+  };
+  firebase.initializeApp(firebaseConfig);
